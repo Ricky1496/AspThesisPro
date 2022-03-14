b7a7cdb70d91282fec065b13aaf180708da9a1f4
Ricky Abraham <rickyabraham43@gmail.com>
2022-03-14 11:55:34 +0530

    created identityUser

---
+++ AspThesisPro/Models/MyIdentityRole.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspThesisPro.Models
+{
+    public class MyIdentityRole
+    {
+    }
+}
---
+++ AspThesisPro/Models/MyIdentityUser.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AspThesisPro.Models
+{
+    public class MyIdentityUser
+        :IdentityUser<Guid>
+    {
+
+        [Display(Name = "Display Name")]
+        [Required(ErrorMessage = "{0} cannot be empty")]
+        [MinLength(2)]
+        [StringLength(60, ErrorMessage = "{0} should have atleast {1} Characters.")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Date of Birth")]
+
+        [Required]
+        [PersonalData]              // for GDPR Compliance
+        [Column(TypeName = "smalldatetime")]
+        public DateTime DateOfBirth { get; set; }
+
+        [Display(Name = "Gender")]
+        [Required]
+        public string Gender { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [DataType(DataType.PhoneNumber)]
+        [Required(ErrorMessage = "Phone number is Required")]
+        public string Phonenumber { get; set; }
+
+        [Display(Name = "Role Type")]
+        [Required]
+        public string Roletype { get; set; }
+    }
+}
